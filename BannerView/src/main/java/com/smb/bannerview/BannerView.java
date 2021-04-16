8a80b3ba9db5bc2c8759a231561001dc844db22e
SMehranB <m.behbahani@live.com>
2021-04-16 22:17:53 +0300

    version 1.0.0

--- BannerView/src/main/java/com/smb/bannerview/BannerView.java
+++ BannerView/src/main/java/com/smb/bannerview/BannerView.java
@@ -10,21 +10,81 @@ import android.graphics.Color;
 import android.graphics.LinearGradient;
 import android.graphics.Paint;
 import android.graphics.Shader;
+import android.graphics.Typeface;
+import android.text.TextPaint;
 import android.util.AttributeSet;
 import android.view.View;
-import android.view.animation.AccelerateDecelerateInterpolator;
 
 import androidx.annotation.ColorInt;
+import androidx.annotation.FontRes;
+import androidx.core.content.res.ResourcesCompat;
 
 public class BannerView extends View {
 
-    public BannerView(Context context){
+    private final float verticalPadding = dpToPixel(8);
+
+    public BannerView(Context context) {
         super(context);
         initAttributes(context, null);
     }
+
     public BannerView(Context context, AttributeSet attributeSet) {
         super(context, attributeSet);
         initAttributes(context, attributeSet);
+    }
+
+    private LinearGradient shader = null;
+
+    long animationDuration = 15000L;
+
+    @ColorInt
+    int shadowColor = 0;
+    float shadowOffSetX = 0f;
+    float shadowOffSetY = 0f;
+    float shadowRadius = 0f;
+
+    private final Paint highlightPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+    String highlightText = "";
+    @ColorInt
+    int highlightColor = 0;
+    boolean highlightUnderlined = false;
+    int highlightStyle = 0;
+    int highlightFont = 0;
+    float highlightSize = 0f;
+    boolean highlightShadowEnabled = false;
+
+    private final TextPaint textPaint = new TextPaint(Paint.ANTI_ALIAS_FLAG | TextPaint.LINEAR_TEXT_FLAG);
+    String message = "";
+    @ColorInt
+    int textColor = 0;
+    float textSize = 0;
+    int textStyle = 0;
+    int textFont = 0;
+    boolean textShadowEnabled = false;
+
+    @ColorInt
+    int gradientStart = Color.GREEN;
+    @ColorInt
+    int gradientEnd = Color.RED;
+
+    private int textX = 0;
+    private int textY = 0;
+
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+
+        textPaint.setShader(shader);
+        if (highlightColor == 0) {
+            highlightPaint.setColor(Color.WHITE);
+            highlightPaint.setShader(shader);
+        }
+
+        if (highlightText == null) {
+            canvas.drawText(message, textX, textY, textPaint);
+        } else {
+            drawSpannedText(canvas);
+        }
     }
 
     private void initAttributes(Context context, AttributeSet attributeSet) {
@@ -32,82 +92,59 @@ public class BannerView extends View {
         TypedArray attr = context.getTheme().obtainStyledAttributes(attributeSet, R.styleable.BannerView, 0, 0);
 
         message = attr.getString(R.styleable.BannerView_android_text);
-        if(message == null) message = "This is just a banner passing by. Nothing special to see here.";
+        if (message == null) message = "This is just an awesome styleable BannerView.";
+        message += " ";
 
-        textSize = (int) attr.getDimension(R.styleable.BannerView_android_textSize, dpToPixel(16));
+        textSize = attr.getDimension(R.styleable.BannerView_android_textSize, dpToPixel(16));
         textColor = attr.getInteger(R.styleable.BannerView_android_textColor, Color.BLACK);
+        textStyle = attr.getInt(R.styleable.BannerView_android_textStyle, Typeface.NORMAL);
+        textFont = attr.getResourceId(R.styleable.BannerView_bv_TextFont, 0);
 
         highlightText = attr.getString(R.styleable.BannerView_bv_highlightText); //May return null
-        highlightColor = attr.getInteger(R.styleable.BannerView_bv_highlightColor, textColor);
+        highlightSize = attr.getDimension(R.styleable.BannerView_bv_highlightSize, textSize);
+        highlightColor = attr.getInteger(R.styleable.BannerView_bv_highlightColor, 0);
+        highlightStyle = attr.getInt(R.styleable.BannerView_bv_highlightStyle, textStyle);
+        highlightFont = attr.getResourceId(R.styleable.BannerView_bv_highlightFont, textFont);
+        highlightUnderlined = attr.getBoolean(R.styleable.BannerView_bv_highlightUnderlined, false);
+
+        shadowColor = attr.getInt(R.styleable.BannerView_android_shadowColor, 0);
+        shadowOffSetX = attr.getFloat(R.styleable.BannerView_android_shadowDx, 0f);
+        shadowOffSetY = attr.getFloat(R.styleable.BannerView_android_shadowDy, 0f);
+        shadowRadius = attr.getFloat(R.styleable.BannerView_android_shadowRadius, 0f);
 
         gradientStart = attr.getInteger(R.styleable.BannerView_bv_gradientStart, 0);
         gradientEnd = attr.getInteger(R.styleable.BannerView_bv_gradientEnd, 0);
 
-        enableGlow = attr.getBoolean(R.styleable.BannerView_bv_enableGlow, false);
+        highlightShadowEnabled = attr.getBoolean(R.styleable.BannerView_bv_enableHighlightShadow, true);
+        textShadowEnabled = attr.getBoolean(R.styleable.BannerView_bv_enableTextShadow, true);
 
-        animationDuration = (long) attr.getInt(R.styleable.BannerView_android_animationDuration, 15000);
+        animationDuration = attr.getInt(R.styleable.BannerView_android_animationDuration, 15000);
+
+        //THIS IS TO SHOW PROPER VIEW SIZES IN THE LAYOUT VIEW
+        textPaint.setTextSize(textSize);
+        highlightPaint.setTextSize(highlightSize);
 
         attr.recycle();
+
     }
 
-    Paint textPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
-    final Paint highlightPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
-    LinearGradient shader = null;
-
-    long animationDuration = 15000L;
-
-    String highlightText = "";
-    @ColorInt
-    int highlightColor = 0;
-
-    String message = "";
-    @ColorInt
-    int textColor = Color.WHITE;
-    int textSize = 16;
-
-    @ColorInt
-    int gradientStart = Color.GREEN;
-    @ColorInt
-    int gradientEnd = Color.RED;
-    boolean enableGlow = false;
-
-    int textX = 0;
-    int textY = 0;
-
-
-    @Override
-    protected void onDraw(Canvas canvas) {
-
-        setLayerType(LAYER_TYPE_SOFTWARE, textPaint);
-
-        if(enableGlow) textPaint.setShadowLayer(textSize / 3f, 0f, 0f, textColor);
-
-        if(shader != null) textPaint.setShader(shader);
-
-        if (highlightText == null) {
-            canvas.drawText(message, textX, textY, textPaint);
-        } else {
-            drawComplexText(canvas);
-        }
-    }
-
-    private void drawComplexText(Canvas canvas) {
+    private void drawSpannedText(Canvas canvas) {
         String[] messageParts = message.split(highlightText);
 
-        for (int i = 0; i < messageParts.length; i++) {
+        for (int i = 0; i < messageParts.length - 1; i++) {
             canvas.drawText(messageParts[i], textX + getNextX(messageParts, i), textY, textPaint);
-            if (i != messageParts.length - 1) {
-                canvas.drawText(highlightText, textX + getNextX(messageParts, i) + textPaint.measureText(messageParts[i]), textY, highlightPaint);
-            }
+            canvas.drawText(highlightText, textX + getNextX(messageParts, i) + textPaint.measureText(messageParts[i]), textY, highlightPaint);
         }
+
+        canvas.drawText(messageParts[messageParts.length - 1], textX + getNextX(messageParts, messageParts.length - 1), textY, textPaint);
     }
 
     private int getNextX(String[] textParts, int stage) {
 
-        if(stage == 0) return 0;
+        if (stage == 0) return 0;
 
         int result = 0;
-        float highlightLength = textPaint.measureText(highlightText);
+        float highlightLength = highlightPaint.measureText(highlightText);
 
         for (int i = 0; i < stage; i++) {
             result += textPaint.measureText(textParts[i]) + highlightLength;
@@ -117,28 +154,101 @@ public class BannerView extends View {
     }
 
     @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+
+        int w = MeasureSpec.getSize(widthMeasureSpec);
+        int h = MeasureSpec.getSize(heightMeasureSpec);
+
+        int hMode = MeasureSpec.getMode(heightMeasureSpec);
+
+        int textHeight = (int) (textPaint.descent() - textPaint.ascent());
+        int highlightHeight = (int) (highlightPaint.descent() - highlightPaint.ascent());
+
+        int minHeight = (int) (Math.max(textHeight, highlightHeight) + verticalPadding * 2 + shadowOffSetY);
+
+        switch (hMode) {
+            case MeasureSpec.EXACTLY:
+                setMeasuredDimension(w, h);
+                break;
+            case MeasureSpec.AT_MOST:
+                setMeasuredDimension(w, Math.min(minHeight, h));
+                break;
+            case MeasureSpec.UNSPECIFIED:
+                setMeasuredDimension(w, minHeight);
+                break;
+        }
+    }
+
+    @Override
     protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
+
+        setTextPaintAttr();
+        setHighlightPaintAttr();
 
         if (gradientStart != 0 && gradientEnd != 0) {
             shader = new LinearGradient(0f, 0f, getWidth(), 0f, gradientStart, gradientEnd, Shader.TileMode.CLAMP);
         }
 
-        Paint.FontMetrics fm = textPaint.getFontMetrics();
-        textY = (int) ((getHeight() / 2) - (fm.ascent + fm.descent) / 2);
+        int textHeight = (int) (textPaint.descent() + textPaint.ascent());
+        int highlightHeight = (int) (highlightPaint.descent() + highlightPaint.ascent());
+
+        textY = (int) ((getHeight() / 2) - (Math.min(textHeight, highlightHeight)) / 2 - shadowOffSetY / 2);
         textX = getWidth();
 
         super.onLayout(changed, left, top, right, bottom);
     }
 
-    public void animateBanner() {
-
-        highlightPaint.setTextSize(dpToPixel(24));
-        highlightPaint.setColor(highlightColor);
-
-        textPaint.setTextSize(dpToPixel(24));
+    private void setTextPaintAttr() {
+        textPaint.setTextSize(textSize);
         textPaint.setColor(textColor);
+        Typeface tf = Typeface.DEFAULT;
+        if (textFont != 0) {
+            tf = ResourcesCompat.getFont(getContext(), textFont);
+        }
+        textPaint.setTypeface(Typeface.create(tf, textStyle));
+        if (textShadowEnabled) textPaint.setShadowLayer(shadowRadius, shadowOffSetX, shadowOffSetY, shadowColor);
+    }
 
-        ValueAnimator va = ValueAnimator.ofInt(getResources().getDisplayMetrics().widthPixels, (int) -textPaint.measureText(message));
+    private void setHighlightPaintAttr() {
+        highlightPaint.setTextSize(textSize);
+        highlightPaint.setColor(highlightColor);
+        Typeface tf = Typeface.DEFAULT;
+        if (highlightFont != 0) {
+            tf = ResourcesCompat.getFont(getContext(), highlightFont);
+        } else if (textFont != 0) {
+            tf = ResourcesCompat.getFont(getContext(), textFont);
+        }
+        highlightPaint.setTypeface(Typeface.create(tf, highlightStyle));
+        highlightPaint.setUnderlineText(highlightUnderlined);
+        highlightPaint.setTextSize(highlightSize);
+        if (highlightShadowEnabled) highlightPaint.setShadowLayer(shadowRadius, shadowOffSetX, shadowOffSetY, shadowColor);
+    }
+
+    private float dpToPixel(int dp) {
+        return dp * getResources().getDisplayMetrics().density;
+    }
+
+    private int getEndPosition() {
+        String[] messageParts = message.split(highlightText);
+
+        int result = 0;
+
+        for (int i = 0; i < messageParts.length - 1; i++) {
+            result += textPaint.measureText(messageParts[i]);
+            result += highlightPaint.measureText(highlightText);
+        }
+
+        result += textPaint.measureText(messageParts[messageParts.length - 1]);
+
+        return result * -1;
+    }
+
+    public void start() {
+
+        int startPos = getResources().getDisplayMetrics().widthPixels;
+        int endPos = getEndPosition();
+
+        ValueAnimator va = ValueAnimator.ofInt(startPos, endPos);
         va.addUpdateListener(valueAnimator -> {
             textX = (int) valueAnimator.getAnimatedValue();
             invalidate();
@@ -146,7 +256,7 @@ public class BannerView extends View {
 
         AnimatorSet textAnimation = new AnimatorSet();
         textAnimation.setDuration(animationDuration);
-        textAnimation.setInterpolator(new AccelerateDecelerateInterpolator());
+        textAnimation.setInterpolator(null);
         textAnimation.addListener(new Animator.AnimatorListener() {
             @Override
             public void onAnimationStart(Animator animator) {
@@ -170,7 +280,165 @@ public class BannerView extends View {
         textAnimation.start();
     }
 
-    private Float dpToPixel(int dp){
-        return dp * getResources().getDisplayMetrics().density;
+    public long getAnimationDuration() {
+        return animationDuration;
+    }
+
+    public void setAnimationDuration(long animationDuration) {
+        this.animationDuration = animationDuration;
+    }
+
+    public void setShadowParams(@ColorInt int color, float xOffset, float yOffset, float radius) {
+        this.shadowColor = color;
+        this.shadowOffSetX = xOffset;
+        this.shadowOffSetY = yOffset;
+        this.shadowRadius = radius;
+
+        requestLayout();
+    }
+
+    public int getShadowColor() {
+        return shadowColor;
+    }
+
+    public float getShadowOffSetX() {
+        return shadowOffSetX;
+    }
+
+    public float getShadowOffSetY() {
+        return shadowOffSetY;
+    }
+
+    public float getShadowRadius() {
+        return shadowRadius;
+    }
+
+    public String getHighlightText() {
+        return highlightText;
+    }
+
+    public void setHighlightColor(int highlightColor) {
+        this.highlightColor = highlightColor;
+    }
+
+    public void setHighlightText(String highlightText, boolean highlightUnderlined) {
+        this.highlightText = highlightText;
+        this.highlightUnderlined = highlightUnderlined;
+    }
+
+    public float getHighlightSize() {
+        return highlightSize;
+    }
+
+    public void setHighlightSize(int size) {
+        this.highlightSize = dpToPixel(size);
+        highlightPaint.setTextSize(highlightSize);
+        requestLayout();
+    }
+
+    public int getHighlightColor() {
+        return highlightColor;
+    }
+
+    public boolean isHighlightUnderlined() {
+        return highlightUnderlined;
+    }
+
+    public int getHighlightStyle() {
+        return highlightStyle;
+    }
+
+    public void setHighlightStyle(int highlightStyle) {
+        this.highlightStyle = highlightStyle;
+    }
+
+    public int getHighlightFont() {
+        return highlightFont;
+    }
+
+    public void setHighlightFont(@FontRes int font) {
+        this.highlightFont = font;
+    }
+
+    public boolean isHighlightShadowEnabled() {
+        return highlightShadowEnabled;
+    }
+
+    public void setHighlightShadowEnabled(boolean highlightShadowEnabled) {
+        this.highlightShadowEnabled = highlightShadowEnabled;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public int getTextColor() {
+        return textColor;
+    }
+
+    public void setTextColor(@ColorInt int color) {
+        this.textColor = color;
+    }
+
+    public float getTextSizeDp() {
+        return textSize / getResources().getDisplayMetrics().density;
+    }
+
+    public void setTextSizeDP(int size) {
+        this.textSize = dpToPixel(size);
+        textPaint.setTextSize(dpToPixel(size));
+        requestLayout();
+    }
+
+    public int getTextStyle() {
+        return textStyle;
+    }
+
+    public void setTextStyle(int style) {
+        this.textStyle = style;
+        Typeface tf = Typeface.DEFAULT;
+        if (textFont != 0) {
+            tf = ResourcesCompat.getFont(getContext(), textFont);
+        }
+        textPaint.setTypeface(Typeface.create(tf, textStyle));
+    }
+
+    public int getTextFont() {
+        return textFont;
+    }
+
+    public void setTextFont(@FontRes int font) {
+        this.textFont = font;
+        Typeface tf = Typeface.DEFAULT;
+        if (textFont != 0) {
+            tf = ResourcesCompat.getFont(getContext(), textFont);
+        }
+        textPaint.setTypeface(Typeface.create(tf, textStyle));
+        requestLayout();
+    }
+
+    public boolean isTextShadowEnabled() {
+        return textShadowEnabled;
+    }
+
+    public void setTextShadowEnabled(boolean textShadowEnabled) {
+        this.textShadowEnabled = textShadowEnabled;
+    }
+
+    public int getGradientStart() {
+        return gradientStart;
+    }
+
+    public void setGradientColors(int gradientStart, int gradientEnd) {
+        this.gradientStart = gradientStart;
+        this.gradientEnd = gradientEnd;
+    }
+
+    public int getGradientEnd() {
+        return gradientEnd;
     }
 }
--- app/src/main/java/com/smb/bannerviewsample/MainActivity.kt
+++ app/src/main/java/com/smb/bannerviewsample/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.smb.bannerviewsample
 
+import android.graphics.Color
+import android.graphics.Typeface
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.smb.bannerview.BannerView
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -9,10 +12,37 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-//        Handler().postDelayed(Runnable {
-            banner.animateBanner()
+        banner1.start()
+        banner2.start()
+        banner3.start()
+        banner4.start()
+        banner5.start()
+        banner6.start()
 
-//        }, 1000)
+        val bv = BannerView(this)
+
+        bv.apply {
+            message = "This is a BannerView with different styleable BannerView properties."
+            setTextSizeDP(16)
+            setGradientColors(Color.GREEN, Color.RED)
+            textStyle = Typeface.NORMAL
+            textFont = R.font.dancing_script
+            textColor = Color.WHITE
+            setShadowParams(Color.LTGRAY, 10f, 10f, 5f)
+            animationDuration = 15000
+            highlightFont = R.font.dancing_script
+            highlightStyle = Typeface.BOLD
+            setHighlightText("BannerView", false)
+            highlightColor = Color.MAGENTA
+            setHighlightSize(18)
+            isHighlightShadowEnabled = true
+            isTextShadowEnabled = false
+        }
+
+//        viewHolder.addView(bv)
+//
+//        bv.start()
+
 
     }
 }
